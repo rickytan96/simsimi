dcf2140af27193a4955ce65bf1ca9a2502413c9e
rickytan96 <49334893+rickytan96@users.noreply.github.com>
2019-04-06 14:54:28 +0700

    Update and rename bot.php to botleaf.php

--- botleaf.php
+++ botleaf.php
@@ -6,8 +6,8 @@ Modified by Ilyasa
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //Your Channel Access Token
-$channelSecret = 'YOUR-CHANNEL-SECRET';//Your Channel Secret
+$channelAccessToken = 'tEo4aeGyY71znzpcYJO4ohNKg/UAB9aroI+WRWKjjoKVSJk0DpuTofZG3e+y3NRFcZ3fggoqHy47fx4ECbKZo9/K6/h44sxd8X5dNpirp16KAzneaIWz3SDeRAFJVPuVkGfDcZ7xURFDCbSvI6EsfQdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
+$channelSecret = 'ad47324b4851fd14bae908104114991d';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -34,7 +34,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
+$key = '63cdf862-ccf6-4a80-aab4-d4bb1f7d5b4b'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
